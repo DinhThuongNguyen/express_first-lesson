aa67cabb5da7c520336eddcafc8eee01531c538f
dinhthuong <dinhthuong092@gmail.com>
2020-04-05 21:04:58 +0700

    commit file index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 1992;
 
 app.get('/user', function(request, response) {
-	response.send("data from server")
+	response.send("data from server .")
 });
 app.listen(port,function(){
 	console.log("port đã được tạo: "+ port)
